d1eb8d62f8385a8b6095a71829ced01c697e9b7e
Andrey Yastrebov <Andrey.Yastrebov@jetbrains.com>
2020-08-26 20:33:04 +0300

    remove rx

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .library(
             name: "XCoordinator",
             targets: ["XCoordinator"]),
-        .library(
-            name: "XCoordinatorRx",
-            targets: ["XCoordinatorRx"]),
+//        .library(
+//            name: "XCoordinatorRx",
+//            targets: ["XCoordinatorRx"]),
         .library(
             name: "XCoordinatorCombine",
             targets: ["XCoordinatorCombine"]),
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1"),
+//        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,14 +29,14 @@ let package = Package(
         .target(
             name: "XCoordinator",
             dependencies: []),
-        .target(
-            name: "XCoordinatorRx",
-            dependencies: ["XCoordinator", "RxSwift"]),
+//        .target(
+//            name: "XCoordinatorRx",
+//            dependencies: ["XCoordinator", "RxSwift"]),
         .target(
             name: "XCoordinatorCombine",
             dependencies: ["XCoordinator"]),
         .testTarget(
             name: "XCoordinatorTests",
-            dependencies: ["XCoordinator", "XCoordinatorRx"]),
+            dependencies: ["XCoordinator", "XCoordinatorCombine"]),
     ]
 )
